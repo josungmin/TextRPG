411335b237cc0102e02a99465fec77c350087a6f
SungMinJo <37390159+josungmin@users.noreply.github.com>
2025-06-23 20:38:58 +0900

    장착 컴포넌트 리펙터링

--- TextRPG/Component/Equipment.cpp
+++ TextRPG/Component/Equipment.cpp
@@ -1,5 +1,6 @@
 #include "Equipment.h"
 #include "../GameInstance.h"
+#include "../Stat/StatDataType.h"
 #include "../Item/EquipableItem.h"
 
 Equipment::Equipment()
@@ -9,89 +10,7 @@ Equipment::Equipment()
 
 Equipment::~Equipment()
 {
-	if (m_weapon != nullptr)
-	{
-		delete m_weapon;
-	}
-
-	if (m_armor != nullptr)
-	{
-		delete m_armor;
-	}
-}
-
-const bool Equipment::Equip(const EquipableItem* item, StatContainer& ownerStatContainer)
-{
-	EquipableItem* equipItem = dynamic_cast<EquipableItem*>(item->Clone());
-	const ModifierContainer& modifierContainer = equipItem->GetModifierContainer();
-
-	switch (item->GetEquipType())
-	{
-		case EquipableItem::EEquipType::Weapon:
-		{
-			if (m_weapon)
-			{
-				ownerStatContainer.RemoveModifierContainer(m_weapon->GetModifierContainer().id);
-				delete m_weapon;
-			}
-
-			m_weapon = equipItem;
-			ownerStatContainer.AddModifierContainer(modifierContainer);
-			return true;
-		}
-		case EquipableItem::EEquipType::Armor:
-		{
-			if (m_armor)
-			{
-				ownerStatContainer.RemoveModifierContainer(m_armor->GetModifierContainer().id);
-				delete m_armor;
-			}
-
-			m_armor = equipItem;
-			ownerStatContainer.AddModifierContainer(modifierContainer);
-			return true;
-		}
-		default:
-		{
-			delete equipItem;
-			return false;
-		}
-	}
-}
-
-const bool Equipment::Unequip(EquipableItem::EEquipType type, StatContainer& statContainer)
-{
-	switch (type)
-	{
-		case EquipableItem::EEquipType::Weapon:
-		{
-			if (m_weapon != nullptr)
-			{
-				statContainer.RemoveModifierContainer(m_weapon->GetModifierContainer().id);
-				delete m_weapon;
-				m_weapon = nullptr;
-				return true;
-			}
-			break;
-		}
-		case EquipableItem::EEquipType::Armor:
-		{
-			if (m_armor != nullptr)
-			{
-				statContainer.RemoveModifierContainer(m_armor->GetModifierContainer().id);
-				delete m_armor;
-				m_armor = nullptr;
-				return true;
-			}
-			break;
-		}
-		default:
-		{
-			break;
-		}
-	}
-
-	return false;
+	equipedItemList.clear();
 }
 
 const bool Equipment::IsEquiped(EquipableItem::EEquipType equipType) const
--- TextRPG/Component/Equipment.h
+++ TextRPG/Component/Equipment.h
@@ -1,8 +1,9 @@
 #pragma once
 #include <memory>
 #include "../Item/EquipableItem.h"
-#include "../Stat/StatDataType.h"
 #include "../Item/ItemInstance.h"
+
+struct StatContainer;
 
 class Equipment
 {
@@ -11,21 +12,11 @@ public:
     virtual ~Equipment();
 
 public:
-    const bool Equip(const EquipableItem* item, StatContainer& ownerStatContainer);
-    const bool Unequip(EquipableItem::EEquipType type, StatContainer& ownerStatContainer);
-    const bool IsEquiped(EquipableItem::EEquipType equipType) const;
-
-    inline const EquipableItem* GetWeapon() const { return m_weapon; }
-    inline const EquipableItem* GetArmor() const { return m_armor; }
-
     const bool EquipItemInstance(const ItemInstance& itemInstance, StatContainer& ownerStatContainer);
     const ItemInstance& UnequipItemInstance(EquipableItem::EEquipType type, StatContainer& ownerStatContainer);
 
     const EquipableItem* GetEquipedItem(EquipableItem::EEquipType equipType) const;
-
-private:
-    EquipableItem* m_weapon = nullptr;
-    EquipableItem* m_armor = nullptr;
+    const bool IsEquiped(EquipableItem::EEquipType equipType) const;
 
 private:
     vector<ItemInstance> equipedItemList;
